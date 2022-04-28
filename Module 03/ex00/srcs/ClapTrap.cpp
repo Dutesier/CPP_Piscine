959ca1a5089144da352d1a8a8cd35cb273fccf8b
Duarte Areias dos reis <dareias-@c2r8s3.42lisboa.com>
2022-04-28 20:39:06 +0100

    Trapping Claps

--- Module 03/ex00/srcs/ClapTrap.cpp
+++ Module 03/ex00/srcs/ClapTrap.cpp
@@ -1,6 +1,7 @@
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap(std::string new_name): name(new_name) {
+    std::cout << "A wild " << name << " appeared!" << std::endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap& orig) {
@@ -8,6 +9,7 @@ ClapTrap::ClapTrap(const ClapTrap& orig) {
     hit_points = orig.hit_points;
 	energy_points = orig.energy_points;
 	attack_damage = orig.attack_damage;
+    std::cout << "A wild " << name << " appeared!" << std::endl;
 }
 
 ClapTrap & ClapTrap::operator=(const ClapTrap& orig) {
@@ -15,15 +17,16 @@ ClapTrap & ClapTrap::operator=(const ClapTrap& orig) {
     hit_points = orig.hit_points;
 	energy_points = orig.energy_points;
 	attack_damage = orig.attack_damage;
+    std::cout << "A wild " << name << " appeared!" << std::endl;
     return (*this);
 }
 
 ClapTrap::~ClapTrap() {
-
+    std::cout << name << " has been destroyed!" << std::endl;
 }
 
 void	ClapTrap::attack(const std::string& target) {
-
+    std::cout << "ClapTrap " << name << " attacked "<< target << " causing " << attack_damage << " points of damage!" << std::endl:
 }
 
 void	ClapTrap::takeDamage(unsigned int amount) {
