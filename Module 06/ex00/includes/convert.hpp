bea441d6e29d7c26e5babe8c69ea29beb3976d3c
sandash <sandash@42.com>
2022-05-13 15:51:37 +0100

    Fun

--- Module 06/ex00/includes/convert.hpp
+++ Module 06/ex00/includes/convert.hpp
@@ -1,12 +1,11 @@
 #ifndef CONVERT_HPP
 # define CONVERT_HPP
 
-# define LOG(x) std::cout << std::setprecision(8) << x << std::endl
+# define LOG(x) std::cout << std::setprecision(4) << x << std::endl
 
 enum {
     INVALID,
     XCHAR,
-    XFLOAT,
     XDOUBLE,
     XINT,
     NANF,
@@ -20,6 +19,10 @@ enum {
 unsigned int get_type(std::string literal);
 bool    is_digit(char c);
 void print_by_type(unsigned int type, std::string& literal);
-
+void print_double(double d);
+void print_char(char c);
+void print_float(float d);
+void print_special(unsigned int type);
+void theHackerWay(std::string &literal);
 
 #endif
--- Module 06/ex00/srcs/main.cpp
+++ Module 06/ex00/srcs/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include <sstream>
+#include <cstdlib>
 #include <iomanip>
+#include <limits>
+#include <limits.h>
 #include "convert.hpp"
 
 int main(int argc, char* argv[]) {
@@ -13,16 +15,66 @@ int main(int argc, char* argv[]) {
     }
     literal = static_cast<std::string>(argv[1]);
     type = get_type(literal);
-    print_by_type(type, literal);
-    LOG("Type: " << type);
+    if (type == INVALID) {
+        LOG("Error: Invalid literal");
+        return (1);
+    }
+    if (type == XDOUBLE) {
+        theHackerWay(literal);
+        return (0);
+    }
+    if (type == XCHAR) {
+        print_char(literal[0]);
+        return (0);
+    }
+    print_special(type);
+
     return (0);
+}
+
+void theHackerWay(std::string &literal) {
+    char *d_err;
+    char *f_err;
+
+    double  d;
+    float   f;
+
+    bool keep_going = false;
+
+    d = strtod(literal.c_str(), &d_err);
+    if (*d_err != 0) {
+        if (*d_err == '.') {
+            if (&literal[literal.find_first_of('.')] != d_err) {
+                keep_going = true;
+            }
+        } else {
+            keep_going = true;
+        }
+    }
+    if (!keep_going) {
+        print_double(d);
+        return ;
+    }
+    keep_going = false;
+    f = strtof(literal.c_str(), &f_err);
+    if (*f_err != 0) {
+        if (*f_err == 'f') {
+            if (&literal[literal.find_first_of('f')] != f_err) {
+                keep_going = true;
+            }
+        } else {
+            keep_going = true;
+        }
+    }
+    if (!keep_going) {
+        print_float(f);
+        return ;
+    }
+    LOG("INVALID INPUT");
 }
 
 unsigned int get_type(std::string literal) {
     int len = literal.length();
-    int count_f = 0;
-    int count_dot = 0;
-    char c;
 
     if (len == 0)
         return INVALID;
@@ -38,110 +90,85 @@ unsigned int get_type(std::string literal) {
         return NINFF;
     if (literal == "inf-")
         return NINF;
-    if (len > 1) {
-        for (int i = 0; i < len; i++) {
-            c = literal[i];
-            if (c != 'f' && c != '.' && !is_digit(c)) {
-                return INVALID;
-            } else if (c == '.') {
-                count_dot++;
-            } else if (c == 'f') {
-                count_f++;
-            }
-        }
-        if (count_dot > 1 || count_f > 1)
-            return INVALID;
-    }
-    if (count_f)
-        return XFLOAT;
-    if (count_dot)
-        return XDOUBLE;
-    if (!isdigit(literal[0]))
+    if (len == 1 && !is_digit(literal[0]))
         return XCHAR;
-    return XINT;
+    return XDOUBLE;
 }
-
 
 
 void print_special(unsigned int type) {
-    LOG("Special: " << type);
+    LOG("char: impossible");
+    LOG("char: impossible");
+    switch (type) {
+        case NANF:
+        case NAN:
+            LOG("float: nanf");
+            LOG("double: nan");
+            break ;
+        case NINFF:
+        case NINF:
+            LOG("float: ninff");
+            LOG("double: ninf");
+            break ;
+        case INFF:
+        case INF:
+            LOG("float: inff");
+            LOG("double: inf");
+            break ;
+    }
+    
 }
 
-void print_char(std::string& literal) {
-    if (!std::isprint(literal[0]))
+void print_char(char c) {
+    if (!std::isprint(c))
         LOG("char: Non displayable");
     else
-        LOG("char: " << literal);
-    LOG("int: " << static_cast<int>(literal[0]));
-    LOG("float: " << static_cast<float>(literal[0]) << ".0f" );
-    LOG("double: " << static_cast<double>(literal[0]) << ".0");
+        LOG("char: " << c);
+    LOG("int: " << static_cast<int>(c));
+    LOG("float: " << static_cast<float>(c) << ".0f" );
+    LOG("double: " << static_cast<double>(c) << ".0");
 }
 
-void print_float(std::string& literal) {
-    std::istringstream iss(literal);    
-    float f;
-
-    iss >> f;
-
-    if (!std::isprint(static_cast<char>(f)))
+void print_double(double d) {
+    if (d > std::numeric_limits<char>::max() || d < std::numeric_limits<char>::min()) {
+        LOG("char: impossible");
+    } else if (!std::isprint(static_cast<char>(d))) {
         LOG("char: Non displayable");
-    else
-        LOG("char: " << static_cast<char>(f));
-    LOG("int: " << static_cast<int>(f));
-    LOG("float: " << f << "f");
-    LOG("double: " << static_cast<double>(f));
-}
-
-void print_double(std::string& literal) {
-    std::istringstream iss(literal);    
-    double d;
-
-    iss >> d;
-
-    if (!std::isprint(static_cast<char>(d)))
-        LOG("char: Non displayable");
-    else
+    } else {
         LOG("char: " << static_cast<char>(d));
-    LOG("int: " << static_cast<int>(d));
-    LOG("float: " << static_cast<float>(d) << "f");
+    }
+
+    if (d > std::numeric_limits<int>::max() || d < std::numeric_limits<int>::min()) {
+        LOG("int: impossible");
+    } else {
+        LOG("int: " << static_cast<int>(d));
+    }
+
+    if ((d > std::numeric_limits<float>::max()) || d < -__FLT_MAX__) {
+        LOG("float: impossible");
+    } else {
+        LOG("float: " << static_cast<float>(d) << "f");
+    }
+
     LOG("double: " << d);
 }
 
-void print_int(std::string& literal) {
-    std::istringstream iss(literal);    
-    int i;
-
-    iss >> i;
-
-    if (!std::isprint(static_cast<char>(i)))
+void print_float(float f) {
+    LOG("Printing float");
+    if (f > std::numeric_limits<char>::max() || f < std::numeric_limits<char>::min()) {
+        LOG("char: impossible");
+    } else if (!std::isprint(static_cast<char>(f))) {
         LOG("char: Non displayable");
-    else
-        LOG("char: " << static_cast<char>(i));
-    LOG("int: " << i);
-    LOG("float: " << static_cast<float>(i) << ".0f");
-    LOG("double: " << static_cast<double>(i) << ".0");
-}
-
-void print_by_type(unsigned int type, std::string& literal) {
-    switch (type) {
-        case INVALID:
-            LOG("Invalid literal");
-            return ;
-        case XCHAR:
-            print_char(literal);
-            break ;
-        case XFLOAT:
-            print_float(literal);
-            break ;
-        case XDOUBLE:
-            print_double(literal);
-            break ;
-        case XINT:
-            print_int(literal);
-            break ;
-        default:
-            print_special(type);
+    } else {
+        LOG("char: " << static_cast<char>(f));
     }
+    if (f > std::numeric_limits<int>::max() || f < std::numeric_limits<int>::min()) {
+        LOG("int: impossible");
+    } else {
+        LOG("int: " << static_cast<int>(f));
+    }
+    LOG("float: " << f << "f");
+    LOG("double: " << static_cast<double>(f));
 }
 
 bool    is_digit(char c) {
